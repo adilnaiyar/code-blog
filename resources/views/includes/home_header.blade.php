d3607378625ea867f9fe0217b86d5b2dd719fa6e
Adil Naiyar <adil.naiyar@gail.com>
2020-04-17 20:18:06 +0530

    modified the routing of home make it as a landing route

--- resources/views/includes/home_header.blade.php
+++ resources/views/includes/home_header.blade.php
@@ -29,7 +29,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{url('home')}}">#CodeHacking</a>
+                <a class="navbar-brand" href="{{route('home')}}">#CodeHacking</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
